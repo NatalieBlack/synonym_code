5220b735a395706ac7110e9da749699173ee6e22
NatalieBlack <natalievictoriablack@gmail.com>
2014-05-18 15:09:36 -0400

    v2: in python using tokenizer and replacing all words instead of just keywords

---
+++ replace.py
@@ -0,0 +1,46 @@
+import sys
+import re
+import json
+import requests
+import tokenize
+
+def synonym(word):
+    try:
+        r = requests.get("http://words.bighugelabs.com/api/2/e47598ad741728aecaa3bd6a5a35630c/" + word + "/json")
+        if r.status_code == 200:
+            data = r.json()
+            results = data[data.keys()[0]]
+            #print results['syn'][0]
+            return results['syn'][0]
+        else:
+           return word
+    except:
+        return word
+
+lang = sys.argv[1]
+langs = ["ruby"]
+if lang not in langs:
+    sys.exit("Invalid language option.  Supported language options include: " + str(langs))
+
+keys = open("keywords/" + lang + ".txt")
+
+srcfn = sys.argv[2]
+src = open(srcfn)
+
+kws = []
+
+for line in keys:
+    kws.append(line.strip())
+
+keys.close()
+
+contents = src.read()
+print contents
+src.close()
+
+g = tokenize.generate_tokens(open(srcfn).readline)
+for code,tok,_,_,_ in g:
+    if code == 1:
+        contents = re.sub("(\W)" + tok + "(\W)", "\g<1>" + synonym(tok) + "\g<2>", contents)
+
+print contents
